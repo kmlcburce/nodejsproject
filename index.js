71cd348df5ccbb593df2bd8660702056729a7eff
burceky_it32021@CEACCS.NET <burceky_it32021@CEACCS.NET>
2020-02-14 20:14:51 -0800

    update

--- index.js
+++ index.js
@@ -6,65 +6,126 @@ var bodyParser = require('body-parser');
 const path = require('path');
 //api
 app.use(express.json());
-const item = [
-    {id: 1, name:"item1"},
-    {id: 2, name:"item2"},
-    {id: 3, name:"item3"}
+const items = [
+    {id: 1, name:"items1"},
+    {id: 2, name:"items2"},
+    {id: 3, name:"items3"}
 ];
 
 //get 
 app.get('/', function(req, res){
+    console.log('main');
     res.sendFile(path.join(__dirname+'/public/index.html'));
 });
-app.get('/api/item ', function(req, res){
-    console.log('DASD');
-    res.send(item);
+app.get('/api/items', function(req, res){
+    console.log('get items');
+    res.send(items);
 });
-app.get('/api/item/:id', function (req, res){
-    const item = item.find(i => i.id === parseInt(req.params.id));
-    if(!item){
-        return res.status(404).send('Item not found');
+app.get('/api/items/:id', function (req, res){
+    console.log('get spc');
+    const item = items.find(i => i.id === parseInt(req.params.id));
+    if(!items){
+        return res.status(404).send('item not found');
     }
     res.send(item);
 });
 //post
-app.post('/api/additem', function(req, res){
+app.post('/api/newitems', function(req, res){
 
-    const { error } = validateItem(req.body); // result.error --- object destructuring
-    if(error){ // Making use of  object destructuring /const { error } = validateCourse(req.body);/
-    // res.status(400).send(result.error.details[0].message); --  not using object destructuring
-    res.status(400).send(error.details[0].message);
-    return;
-    item.item = req.body.item;
-    res.send(item);
-}   
+    console.log('post');
+    const { error } = validateItems(req.body.name);
+
+    if(error){
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const data = {
+        id: items.length + 1,
+        name: req.body.itemname
+    };
+
+    items.push(data);
+    res.send(items); 
 });
 //del
-app.delete('/api/item/:id', function(req, res){
+app.delete('/api/items/:id', function(req, res){
     // Look up course id
-    const item = item.find(i => i.id === parseInt(req.params.id));
-    if(!item) {
-        res.status(404).send('Item id not found');
+    const item = items.find(i => i.id === parseInt(req.params.id));
+    if(!items) {
+        res.status(404).send('items id not found');
         return;
     }
 
     // Delete
-    const index = item.indexOf(item);
-    courses.splice(index, 1);
+    const index = items.indexOf(items);
+    items.splice(index, 1);
 
     // Return the same course
     res.send(item);
 });
-function validateItem(item){
+//put
+app.put('/api/items/:id', urlencodedParser, function(req,res){
+    // Look up the courses
+    // if not existing, return 404
+    const item = items.find(i => i.id === parseInt(req.params.id));
+    if(!item){
+        return res.status(404).send('Course id not found');
+    }
+
+    // Validate
+    // If invalid, return 400
+
+    // Redunduncy in validation
+    // const schema = {
+    //     name: Joi.string().min(3).required() 
+    // };
+
+    // const result = Joi.validate(req.body, schema);
+
+    // Making use of validateCourse() 
+
+    // const result = validateCourse(req.body);
+    const { error } = validateItems(req.body); // result.error --- object destructuring
+
+    // result.error --- not using object destructuring
+    if(error){ // Making use of  object destructuring /const { error } = validateCourse(req.body);/
+        // res.status(400).send(result.error.details[0].message); --  not using object destructuring
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    
+    // Update course
+    item.name = req.body.itemname;
+
+    // Return updated course
+    res.send(item);
+});
+//validate
+function validateItems(items){
+    console.log('val');
     const schema = {
-        name: Joi.string().min(3).required() 
+        name: Joi.string().min(3).required()
     };
 
-    return Joi.validate(item, schema);
+    return Joi.validate(items, schema);
 }
+//listen
 app.listen(port);
 //body parser ??
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.post('/api/add', urlencodedParser, function (req, res) {
-    res.send('Added item: ' + req.body.item)
+    console.log('add');
+    const { error } = validateItems(req.body.name);
+
+    if(error){
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const data = {
+        id: items.length + 1,
+        name: req.body.itemname
+    };
+
+    items.push(data);
+    res.send(items); 
   })
